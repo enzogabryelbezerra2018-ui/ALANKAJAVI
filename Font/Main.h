5c2c3d04afd59658b9f5a647b662344825e64507
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-10 21:36:43 -0300

    Create Main.h

---
+++ Font/Main.h
@@ -0,0 +1,26 @@
+#define LETRA_Q /** THE QWERT */
+#define LETRA_W /** THE QWERT */
+#define LETRA_E /** THE QWERT */
+#define LETRA_R /** THE QWERT */
+#define LETRA_T /** THE QWERT */
+#define LETRA_Y
+#define LETRA_U
+#define LETRA_I
+#define LETRA_O
+#define LETRA_P
+#define LETRA_A
+#define LETRA_S
+#define LETRA_D
+#define LETRA_F
+#define LETRA_G
+#define LETRA_H
+#define LETRA_J
+#define LETRA_K
+#define LETRA_L
+#define LETRA_Z
+#define LETRA_X
+#define LETRA_C
+#define LETRA_V
+#define LETRA_B
+#define LETRA_N
+#define LETRA_M
